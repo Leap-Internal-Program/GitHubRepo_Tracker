83d1c46ff7840f5e88043bb9a20d60da5d60649e
Daisy Arusey <v-daarusey@microsoft.com>
2024-01-24 17:18:41 +0300

    Add RepositoryListModel

---
+++ GitHubRepoTrackerFE_Blazor/Models/RepositoryListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubRepoTrackerFE_Blazor.Models
+{
+    public class RepositoryListModel
+    {
+        public List<Repository>? repositories { get; set; }
+        public List<Topic>? topics { get; set; }
+        public List<Language>? languages { get; set; }
+
+    }
+}
