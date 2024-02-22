75a576e5f01b16944fd4c72d2938067a3c4a3fa6
Daisy Arusey <v-daarusey@microsoft.com>
2024-02-22 17:12:06 +0300

    Change base url

--- GitHubRepoTrackerFE_Blazor/Program.cs
+++ GitHubRepoTrackerFE_Blazor/Program.cs
@@ -8,7 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7270/") });
+
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://gitrepobemfidahy52nqmg.azurewebsites.net/") });
 builder.Services.AddSingleton<IApiAuthService, ApiAuthService>();
 builder.Services.AddSingleton<IRepoService, RepoService>();
 builder.Services.AddSingleton<ILanguageService, LanguageService>();
